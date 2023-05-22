a392a3f373ad93dad297fccecffe11c158d8bd71
Pre-Class <pre-class@swzs-iMac.lan>
2023-05-22 15:52:20 +0800

    activity 2:Get Fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,10 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+name={first_name: 'Brendon', middle_name: 'Sison', last_name: 'Macapaz'}
+def get_fullname(fullname)
+  return "#{fullname[:first_name]} #{fullname[:middle_name]} #{fullname[:last_name]}"
+end
+
+p get_fullname(name)
