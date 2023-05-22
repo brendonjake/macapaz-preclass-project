53cc0677a0822536a50d3fc703963c35aa1bc73d
Pre-Class <pre-class@swzs-iMac.lan>
2023-05-22 15:31:10 +0800

    Calculate bmi

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,7 +1,4 @@
-# TODO: EXERCISE 1
-#  BMI is weight in kilograms divided by the result of height in meters raised to 2.
-#  Write a function that calculates BMI.
-#  Print on console the result.
+
 
 def calculate_bmi(weight, height)
   bmi = weight / height**2
