fb3a4b252ee5a76838f2af2696b3e39025ae074b
Pre-Class <pre-class@swzs-iMac.lan>
2023-05-22 15:51:02 +0800

    activity 1: Get average adult age

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,18 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def average_adult_age(ages)
+  adults = []
+  adult_age_sum=0
+  ages.each do |age|
+    if age >= 18
+      adults << age
+      adult_age_sum=adult_age_sum+age
+    end
+  end
+
+  return adult_age_sum.to_f/adults.length
+end
+
+p average_adult_age([14,15,25,28,45])
